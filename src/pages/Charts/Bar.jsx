c4c6a3dea777327b5b77aa418decb60cc4d6fff8
unknown <dantim1974@hotmail.com>
2023-11-06 15:03:52 +0100

    code source

---
+++ src/pages/Charts/Bar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, ColumnSeries, Category, Tooltip, DataLabel, Legend } from '@syncfusion/ej2-react-charts';
+import { Header } from '../../components'; 
+import { barCustomSeries, barPrimaryXAxis, barPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+
+
+const Bar = () => {
+  const { currentMode } = useStateContext();
+  return (
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+    <Header category='Bar' title='Inflation Rate in Percentage'/>
+      <ChartComponent id='bar-chart' height='420px' primaryXAxis={barPrimaryXAxis} primaryYAxis={barPrimaryYAxis} chartArea={{border: { width: 0 }}} tooltip={{ enable: true }} background={currentMode === 'Dark' ? '#33373E' : '#fff'}>
+        
+        <Inject services={[ColumnSeries, DataLabel, Legend, Category, Tooltip]} />
+        <SeriesCollectionDirective>
+          {barCustomSeries.map((item, index)=> <SeriesDirective key={index} {...item} />)}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  )
+}
+
+export default Bar
